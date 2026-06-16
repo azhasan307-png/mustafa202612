f64033f829cd91686af517bc1db519e038ba3c15
azhasan307-png <azhasan307@gmail.com>
2026-06-16 14:30:03 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -15,4 +15,4 @@ app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-app.run_pollinghttps://github.com/new/import
+app.run_polling().
